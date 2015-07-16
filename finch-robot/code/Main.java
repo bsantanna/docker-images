8a2137a1b3a9499bc5384669f78c496e86357716
Bruno Sant'Anna <mail@bsantanna.me>
2015-07-16 13:06:07 -0300

    finch docker image prototype

---
+++ finch-robot/code/Main.java
@@ -0,0 +1,22 @@
+import edu.cmu.ri.createlab.terk.robot.finch.Finch;
+
+/**
+ * Created by:
+ * Date:
+ * A starter file to use the Finch
+ */
+
+public class Main
+   {
+   public static void main(final String[] args)
+      {
+      // Instantiating the Finch object
+      Finch myFinch = new Finch();
+
+      // Write some code here!
+
+      // Always end your program with finch.quit()
+      myFinch.quit();
+      System.exit(0);
+      }
+   }
