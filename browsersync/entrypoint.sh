a50e96f83ac86c79d73efd659c849cd53f8f6f5d
Bruno Sant'Anna <mail@bsantanna.me>
2016-08-19 19:03:17 -0300

    weinre funcionando dentro do container

--- browsersync/entrypoint.sh
+++ browsersync/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-node proxy.js
+node proxy.js &
+node_modules/weinre/weinre --httpPort 3002 --boundHost -all-
--- browsersync/https/entrypoint.sh
+++ browsersync/https/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 node ssl_proxy.js &
-node proxy.js
+node proxy.js &
+node_modules/weinre/weinre --httpPort 3002 --boundHost -all-
