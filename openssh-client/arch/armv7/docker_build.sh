e4fe622be79caeab85f9711d7348e1e5ba4583e8
Bruno Sant'Anna <mail@bsantanna.me>
2016-12-29 17:31:34 -0200

    Refatoração para contemplar diferentes arquiteturas

--- openssh-client/arch/armv7/docker_build.sh
+++ openssh-client/arch/armv7/docker_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t "bsantanna/openssh-client-armv7" ..
+docker build -t "bsantanna/openssh-client-armv7" ../../
--- openssh-client/arch/x86_64/docker_build.sh
+++ openssh-client/arch/x86_64/docker_build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker build -t "bsantanna/openssh-client-x86_64" ..
+docker build -t "bsantanna/openssh-client-x86_64" ../../
