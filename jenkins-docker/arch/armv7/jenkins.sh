58cade2368d5edc7129c28a0383fc6664ae7375f
Bruno Sant'Anna <mail@bsantanna.me>
2017-01-03 00:40:56 -0200

    voltando a versao anterior

--- jenkins-docker/arch/armv7/jenkins.sh
+++ jenkins-docker/arch/armv7/jenkins.sh
@@ -19,8 +19,52 @@ if [[ $# -lt 1 ]] || [[ "$1" == "--"* ]]; then
     jenkins_opts_array+=( "$item" )
   done < <([[ $JENKINS_OPTS ]] && xargs printf '%s\0' <<<"$JENKINS_OPTS")
 
-  exec java -XX:PermSize=512M -XX:MaxPermSize=640M -Xmn128M -Xms512M -Xmx640M -Djava.util.logging.config.file=/usr/share/jenkins/log.properties "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
+  exec "${java_opts_array[@]}" -jar /usr/share/jenkins/jenkins.war "${jenkins_opts_array[@]}" "$@"
 fi
 
 # As argument is not jenkins, assume user want to run his own process, for example a `bash` shell to explore this image
 exec "$@"
+
+#!/bin/bash
+
+# First, make sure that cgroups are mounted correctly.
+CGROUP=/sys/fs/cgroup
+
+[ -d $CGROUP ] ||
+  mkdir $CGROUP
+
+mountpoint -q $CGROUP ||
+  mount -n -t tmpfs -o uid=0,gid=0,mode=0755 cgroup $CGROUP || {
+    echo "Could not make a tmpfs mount. Did you use -privileged?"
+    exit 1
+  }
+
+# Mount the cgroup hierarchies exactly as they are in the parent system.
+for SUBSYS in $(cut -d: -f2 /proc/1/cgroup)
+do
+  [ -d $CGROUP/$SUBSYS ] || mkdir $CGROUP/$SUBSYS
+  mountpoint -q $CGROUP/$SUBSYS ||
+    mount -n -t cgroup -o $SUBSYS cgroup $CGROUP/$SUBSYS
+done
+
+# Now, close extraneous file descriptors.
+pushd /proc/self/fd
+for FD in *
+do
+  case "$FD" in
+  # Keep stdin/stdout/stderr
+  [012])
+    ;;
+  # Nuke everything else
+  *)
+    eval exec "$FD>&-"
+    ;;
+  esac
+done
+popd
+
+
+rm -fr /var/run/docker*
+service docker restart
+
+exec java java -XX:PermSize=512M -XX:MaxPermSize=640M -Xmn128M -Xms512M -Xmx640M -Djava.util.logging.config.file=/opt/jenkins/data/log.properties -jar /opt/jenkins/jenkins.war --prefix=/jenkins
