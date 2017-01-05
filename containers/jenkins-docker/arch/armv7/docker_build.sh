eb6979a1f9d21a554a88f85669502c078f10e32b
Bruno Sant'Anna <mail@bsantanna.me>
2017-01-05 14:24:22 -0200

    Desistência de jenkins em armv7

--- containers/jenkins-docker/arch/armv7/docker_build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker build -t "bsantanna/jenkins-docker-armv7" . # Image does not follow default hierarchy
--- containers/jenkins-docker/arch/armv7/docker_push.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker push "bsantanna/jenkins-docker-armv7"
