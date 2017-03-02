6389cdfd4ba0a5a90847d00df6197ea9a9485a60
bsantanna <bruno@btech.software>
2017-03-02 14:45:20 -0300

    Script reutilizavel

--- scripts/manifest_push.sh
+++ scripts/manifest_push.sh
@@ -1,22 +1,28 @@
 #!/usr/bin/env bash
 
-function showHelp {
+function showHelpEnv {
     printf "\n\t manifest-tool needs DOCKER_HUB_USERNAME and DOCKER_HUB_PASSWORD variables set... \n\n"
+}
+
+function showHelpArgs {
+    printf "Missing manifest file argument. \n\n\t Usage: $0 manifest-file.yml\n\n"
 }
 
 function manifestPush {
     manifest-tool \
         --username ${DOCKER_HUB_USERNAME} \
         --password ${DOCKER_HUB_PASSWORD} \
-        push from-spec jenkins.yml
+        push from-spec "$1"
 }
 
 if [ -z "${DOCKER_HUB_USERNAME}" ]; then
-    showHelp
+    showHelpEnv
 elif [ -z "${DOCKER_HUB_PASSWORD}" ]; then
-    showHelp
+    showHelpEnv
+elif [ -z "$1" ]; then
+    showHelpArgs
 else
-    manifestPush
+    manifestPush $*
 fi
 
 
