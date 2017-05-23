e932720dd23868f2801301f5e4f6c9cf3a21059e
bsantanna <bruno@btech.software>
2017-05-23 17:31:28 -0300

    smb fixes

--- containers/smb/scripts/provision/groupadd_batch.sh
+++ containers/smb/scripts/provision/groupadd_batch.sh
@@ -4,5 +4,7 @@ for line in `cat /config/users.txt`
 do
   username=`echo ${line} | cut -f1 -d:`
   groupname=`echo ${line} | cut -f3 -d:`
+
+  groupadd ${groupname} || true
   usermod -aG ${groupname} ${username}
 done
