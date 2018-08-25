bf62abe4e02946b9624db69f5b7fdcab2e3d0ab4
bsantanna <bruno@btech.software>
2018-08-25 12:33:32 +0200

    ddclient armhf

---
+++ images/clients/ddclient/arch/amhf/docker_build.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+docker pull "bsantanna/ddclient-armhf"
+docker build -t "bsantanna/ddclient-armhf" ../../
---
+++ images/clients/ddclient/arch/amhf/docker_push.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker push "bsantanna/ddclient-armhf"
