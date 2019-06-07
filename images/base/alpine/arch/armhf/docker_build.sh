9ddb8f2c11fb94aaaa5e8e2f3100bedba77ada26
bsantanna <bruno@btech.software>
2019-06-08 00:41:06 +0200

    continues refactoring  of entire image tree

--- images/base/alpine/arch/armhf/docker_build.sh
+++ images/base/alpine/arch/armhf/docker_build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo "docker pull 'bsantanna/alpine'" | bash -x
-echo "docker build -t 'bsantanna/alpine-armhf' ../../" | bash -x
+echo "docker build -t 'bsantanna/alpine:armhf' ../../" | bash -x
