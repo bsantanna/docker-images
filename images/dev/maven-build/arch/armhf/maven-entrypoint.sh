2164fe7e10dd2f189cbf32660ec4e06daeb11f44
Bruno Sant'Anna <bruno@btech.software>
2020-02-28 19:54:31 +0100

    Maven Build Image with Deployment Target

--- images/dev/maven-build/arch/armhf/maven-entrypoint.sh
+++ images/dev/maven-build/arch/armhf/maven-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo "mvn ${JAVA_OPTS} --settings  /etc/maven/settings.xml clean install"| bash -x
+echo "mvn ${JAVA_OPTS} --settings  /etc/maven/settings.xml clean install deploy"| bash -x
